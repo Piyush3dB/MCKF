c1f4580a16e0bb6791b0940c48766a0b726810cb
Piyush Singh <epiyusi@fulmar.hw.etv.eld.ericsson.se>
2015-09-30 15:52:11 +0100

    Added comments

--- MCKF.m
+++ MCKF.m
@@ -27,22 +27,41 @@ b=0;             %initial iteration number
 
 delta = 2;       %kernel bandwidth
 
+epsilon = 1e-6;
+
 % Now iterate
-while (norm((x3-x2)/norm(x2))>=1e-6)&&(b<=10000)
-    x2 = x3;  
-    e  = D - W*x2;      %error
+while (norm((x3-x2)/norm(x2))>=epsilon && (b<=10000))
+    x2=x3;
+
+    %error
+    e  = D - W*x2;
     
+    % Correntropy calculation
     for a = 1:n+m
         G(a) = exp(-(e(a,:)^2)/(2*delta^2));
     end
 
+    % Diagonalise Correntropies
     C   = diag(G);
-    P11 = B(1:n,1:n)*inv(C(1:n,1:n))*B(1:n,1:n)';
-    R1  = B((n+1):(n+m),(n+1):(n+m))*inv(C((n+1):(n+m),(n+1):(n+m)))*B((n+1):(n+m),(n+1):(n+m))';
+
+    % 'Measured' covariance update
+    P11 = B(1:n,1:n)                 * inv(C(1:n,1:n))                 * B(1:n,1:n)';
+    
+    % Propagated covariance update
+    R1  = B((n+1):(n+m),(n+1):(n+m)) * inv(C((n+1):(n+m),(n+1):(n+m))) * B((n+1):(n+m),(n+1):(n+m))';
+    
+    % Kalman gain
     K1  = P11*H'*inv(H*P11*H'+R1);
+    
+    % posterior mean update
     x3  = x1+K1*(z-H*x1);
+
+    % loop counter
     b   = b+1;
 end
 
-x = x3;      %optimal estimated state of current time step
-P = (eye(n)-K1*H)*P1*(eye(n)-K1*H)'+K1*R*K1'; %estimated state covariance of current time step
+% optimal estimated state of current time step
+x = x3;
+
+% estimated state covariance of current time step
+P = (eye(n)-K1*H)*P1*(eye(n)-K1*H)'+K1*R*K1';
